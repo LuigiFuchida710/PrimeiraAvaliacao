74a8f717e0d69b9aa6ab41e57af7448645c727e3
LuigiFuchida710 <luigifuchida@icloud.com>
2025-05-22 17:54:32 -0300

    Add files via upload

---
+++ TestaTela.java
@@ -0,0 +1,13 @@
+package login;
+
+import javax.swing.JFrame;
+
+public class TestaTela extends JFrame{
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) {
+		TelaLogin t = new TelaLogin();
+		
+		t.abreTela();
+	}
+}
